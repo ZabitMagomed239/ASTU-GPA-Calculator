9deae4af67374544c7e2dce527c7379aa6e39376
Magomed <samuelangasu3@gmail.com>
2025-06-14 19:56:51 +0300

    Add files via upload
    This Python script is specifically developed for ASTU's students. If you are from different university and want to use this program you are required to customize the script according to your campus subjects.

---
+++ GPA.py
@@ -0,0 +1,34 @@
+#ASTU GPA Calculator
+subjects = ["Applied mathematics II", "Statistics", "Emerging", "Logic", "Biology", "Biology Lab", "Physics Lab", "Chemistry Lab"]
+Points = []
+chrs = []
+
+for x in subjects:
+   Grade_Pt = float(input(f"\nEnter point of {x}: "))
+   chr = int(input(f"Enter credit hour of {x}: "))
+   Point = Grade_Pt * chr
+   print(f"Point of {x} is {Point}")
+   Points.append(Point)
+   chrs.append(chr)
+
+
+print(f"\nThe points you got are: {Points}")
+print("\n Sum of the points is: " + str(sum(Points)))
+print("\n################################\n")
+
+print("Your GPA is: " + str(sum(Points)/sum(chrs)))
+print("\n################################")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
